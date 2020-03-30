e734e23b2f6aa94eea3f149c527001383a164b06
YusofGotboudine <yusof95@hotmail.com>
2020-03-30 13:39:22 +0800

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW SOMETHING HAPPENED";
-  return output;
+var inputHappened = function (currentInput) {
+  console.log(currentInput);
+  console.log("WOW SOMETHING HAPPENED");
+  //display(currentInput);
+  return currentInput;
+};
+
+var display = function (data) {
+  var hOne = document.getElementById("display");
+  hOne.innerText = data.toString();
+  console.log(hOne);
 };
