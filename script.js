f1546628486905b769ae2a06b696bd9f4d06930f
YusofGotboudine <yusof95@hotmail.com>
2020-04-01 08:08:27 +0800

    MVP with checkpoint further

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("hello script js");
 
 var count = 0;
-console.log("Question number " + (count + 1));
+var maxOptions = 4;
 
 var questionsArr = [{ question: "What does 2 + 2 = ?", options: ["2", "4", "6", "8"], answer: "4" },
 { question: "What is the capital of Ethiopia?", options: ["Addis Ababa", "Lagos", "Mogadishu", "Dakar"], answer: "Addis Ababa" },
@@ -11,39 +11,86 @@ var maxRounds = questionsArr.length;
 console.log("Maximum Rounds are: " + maxRounds);
 
 var displayBox = document.getElementById("displayBox");
+var optionsDisplay = document.getElementById("options");
+var optionButtons = [];
 
-var display = function (count) {
+var displayQn = function (count) {
     if (count == maxRounds) {
-        displayBox.textContent = "Congratulations!";
+        displayBox.textContent = "Congratulations!"; //overall win condition
+        removeOptions();
     } else {
-        displayBox.textContent = questionsArr[count].question;
-        console.log(displayBox.textContent);
+        displayBox.textContent = questionsArr[count].question;//display qn
+        console.log("Question number " + (count + 1));
+        console.log(displayBox.textContent);//display options
         for (var j = 0; j < questionsArr[count].options.length; j++) {
             document.getElementById("button" + j).innerHTML = questionsArr[count].options[j];
             console.log(questionsArr[count].options[j]);
         }
+
     };
 };
-display(count);
+displayQn(count);
+
+var removeOptions = function (event) {
+    for (var j = 0; j < maxOptions; j++) { //removes options buttons
+        button = document.getElementById("button" + j);
+        optionButtons.push(button);
+        optionsDisplay.removeChild(button);
+    };
+};
+
+var addOptions = function (event) {
+    for (var j = 0; j < maxOptions; j++) { //removes options buttons
+        button = optionButtons.shift();
+        optionsDisplay.appendChild(button);
+    };
+};
+
+var nextQnButton = function (event) {
+    var nextQnButton = document.createElement("button");
+    nextQnButton.id = "next-qn";
+    nextQnButton.textContent = "Next Question";
+    nextQnButton.type = "button";
+    // document.getElementById("next-qn").onclick = function () { nextQn() };
+    optionsDisplay.appendChild(nextQnButton);
+};
+
+var nextQn = function (event) {
+    document.getElementById("next-qn").outerHTML = "";
+    addOptions();
+    displayQn(count);
+};
+
+var winCondition = function (event) {
+    displayBox.textContent = "Correct Answer!";
+    removeOptions();
+    nextQnButton();//adds next qn button
+    count++;
+};
+
+var loseCondition = function (event) {
+    displayBox.textContent = "Good Attempt!";
+    removeOptions();
+};
 
 var answerCheck = function (currentInput) {
     var correctAns = questionsArr[count].answer;
-    if (currentInput == correctAns) {
-        count++;
-        console.log(count);
-        display(count);
+    if ((currentInput == correctAns)) {//correct answer
+        console.log("Correct Answer");
+        winCondition();//display win and remove buttons add next qn button
+        // if () {
+
+        // };
+
     }
-    else {
-        displayBox.textContent = "Good Attempt!";
+    else {                          //wrong answer
+        console.log("Wrong Answer");
+        loseCondition();//display lose and remove options
     };
 };
 
 function onClick(chosenAnswer) {
-    if (count < maxRounds) {
-        console.log(chosenAnswer);
-        answerCheck(chosenAnswer);
-        var f = document.getElementsByClassName("div.options");
-        f.style.visibility = "hidden";
-    }
+    answerCheck(chosenAnswer);
+    // nextQn();
 };
 
